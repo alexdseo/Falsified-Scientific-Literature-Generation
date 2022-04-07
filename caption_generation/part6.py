8a491904d1c0d6a895a2a2f61ba8b7dc00ed0f8e
Audrey Lin <auderoylin@gmail.com>
2022-04-07 15:47:30 -0700

    Update LaTeX, separate Notebooks, compress images

--- caption_generation/part6.py
+++ caption_generation/part6.py
@@ -1,5 +1,5 @@
 import os
 
 for i in range(0,500):
-	command='curl -X POST "localhost:8764/inception/v3/caption/image" --data-binary @generated_images/samples_8_' + str(i) + '.png >> out.out'
+	command='curl -X POST "localhost:8764/inception/v3/caption/image" --data-binary @generated_images/samples_16_' + str(i) + '.png >> out.out'
 	os.system(command)
